4f29b87e4dd3260345e4bcb387b19ed3fe11caa1
Will Essilfie <wke2102@columbia.edu>
2016-05-21 12:20:07 -0700

    trying new test

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ def upload_file():
 	if file and allowed_file(file.filename):
 		filename = secure_filename(file.filename)
 		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		return redirect(url_for('uploaded_file', filename = filename))
+		return render_template('imagePage.html', filename = "/images/wallpapers/" + filename[1:] )
+		#return redirect(url_for('uploaded_file', filename = filename))
 	else:
 		return render_template('index.html')
 
